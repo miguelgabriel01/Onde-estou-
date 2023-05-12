4ee8a9994d55649573458ce513ff7b65c46f000a
enrell <enrellsa10@gmail.com>
2023-05-12 10:20:03 -0300

    feature: Utilizando Composition API

--- ondeestoufrontend/src/App.vue
+++ ondeestoufrontend/src/App.vue
@@ -1,12 +1,25 @@
-<template>
-  <router-view/> 
-</template>
+<script setup>
+  import { ref, onMounted } from 'vue'
+  /* 
+  Composition API é uma evolução da Options API e oferece mais flexibilidade, 
+  modularidade e controle sobre a lógica do componente.
+  Embora a Options API ainda seja amplamente suportada e seja uma opção válida, 
+  a Composition API é recomendada para novos projetos e pode trazer benefícios 
+  significativos para a organização e a manutenção do código.
 
-<script>
-export default {
-  name: 'App'
-}
+  Veja o resumo aqui https://docs.google.com/document/d/1ABheqNifizTqMEt3XMDVx9hsgfUtSQX0QbTpF2LUics/edit?usp=sharing
+  recomendo que estudem essa foma moderna de trabalhar com o vue.
+  */
+
+  onMounted(() => {
+    console.log('Hello, World');
+  })
 </script>
+
+<template>
+  <router-view/>
+
+</template>
 
 <style>
 #app {
